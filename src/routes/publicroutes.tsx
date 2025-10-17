43dece7f7fc53fee0043e4819e0300c527411d62
Lia Nguyen <lnguyen9520@gmail.com>
2025-10-16 20:23:56 -0700

    Set-up Homepage NF404-83

--- src/routes/publicroutes.tsx
+++ src/routes/publicroutes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Homepage from "../pages/public/Homepage";
 
 
 const PublicRoutes = () => (
 
     <Routes>
+        <Route path="/" element={<Homepage />} />
     </Routes>
 
 );
