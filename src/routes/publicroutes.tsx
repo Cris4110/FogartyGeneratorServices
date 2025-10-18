3fe7eac03a4db221a0037b63ff00fe13045498a6
cedwards0403 <163354917+cedwards0403@users.noreply.github.com>
2025-10-17 17:55:50 -0700

    Update publicroutes.tsx
    Added route to the Services page to publicroutes.tsx

--- src/routes/publicroutes.tsx
+++ src/routes/publicroutes.tsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/public/Homepage";
-
+import Servicespage from "../pages/public/Servicespage";
 
 const PublicRoutes = () => (
-
-    <Routes>
-        <Route path="/" element={<Homepage />} />
-    </Routes>
-
+  <Routes>
+    <Route path="/" element={<Homepage />} />
+    <Route path="/services" element={<Servicespage />} />
+  </Routes>
 );
 
 export default PublicRoutes;
