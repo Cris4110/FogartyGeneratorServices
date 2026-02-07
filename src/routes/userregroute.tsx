f0a9953731970f4c50cab66a8d258992c272be9a
Heer <heertandel@csus.edu>
2026-02-06 20:43:54 -0800

    NF404-150: Add LeaveReview page with UI, star rating, and protected route

--- src/routes/userregroute.tsx
+++ src/routes/userregroute.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/public/UserRegistration";
 import LeaveReview from "../pages/public/LeaveReview";
-import { ProtectedRoute } from "./protectedroute";
+import { protectedroute } from "./protectedroute";
 
 const UserReg: React.FC = () => (
     <Routes>
       <Route path="/userreg" element={<Dashboard />} />
-      <Route path="/leavereview" element={<ProtectedRoute component={LeaveReview} />} />
+      <Route path="/leavereview" element={<protectedroute component={LeaveReview} />} />
     </Routes>
 
 );
